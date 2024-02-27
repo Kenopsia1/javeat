e0b6bad4761e0e1f5e3a2c701049ea062ab97117
ErnestoPM <ernestoparadisimiconi@gmail.com>
2024-02-27 13:49:22 +0100

    Util

---
+++ src/main/java/com/generation/javeat/util/UniversalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.generation.javeat.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class UniversalExceptionHandler 
+{
+     @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> gestisciEccezioneConversioneJson(HttpMessageNotReadableException e)//catch(HttpMessageNotReadableException e)
+    {
+        return new ResponseEntity<String>("Errore di conversione da Json a Oggetto", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> gestisciEccezionPathVariable(MethodArgumentTypeMismatchException e)
+    {
+        return new ResponseEntity<String>("Errore nella path variable", HttpStatus.NOT_ACCEPTABLE);
+    }
+}
