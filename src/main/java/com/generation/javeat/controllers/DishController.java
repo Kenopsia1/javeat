7f198e9b6398f80d732cb439229202efe6e1c1bd
ErnestoPM <ernestoparadisimiconi@gmail.com>
2024-02-27 15:11:08 +0100

    Controller1

--- src/main/java/com/generation/javeat/controllers/DishController.java
+++ src/main/java/com/generation/javeat/controllers/DishController.java
@@ -1,5 +1,16 @@
 package com.generation.javeat.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.javeat.model.dtoservices.DishConverter;
+import com.generation.javeat.model.repositories.DishRepository;
+
+@RestController
 public class DishController {
 
+    @Autowired
+    DishConverter dConv;
+    @Autowired
+    DishRepository dRepo;
 }
