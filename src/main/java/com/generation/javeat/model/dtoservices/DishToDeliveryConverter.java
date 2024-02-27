f502a997e63ac25c95b1f3a56d29f1f77750e067
ErnestoPM <ernestoparadisimiconi@gmail.com>
2024-02-27 14:37:21 +0100

    DtoBase

--- src/main/java/com/generation/javeat/model/dtoservices/DishToDeliveryConverter.java
+++ src/main/java/com/generation/javeat/model/dtoservices/DishToDeliveryConverter.java
@@ -3,9 +3,36 @@ package com.generation.javeat.model.dtoservices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.generation.javeat.model.dto.dishtodelivery.DishToDeliveryDtoR;
+import com.generation.javeat.model.dto.dishtodelivery.DishToDeliveryDtoWFull;
+import com.generation.javeat.model.entities.DishToDelivery;
 import com.generation.javeat.model.repositories.DishToDeliveryRepository;
 @Service
 public class DishToDeliveryConverter {
     @Autowired
     DishToDeliveryRepository dtdRepo;
+    
+        public DishToDelivery dtoRToPerson(DishToDeliveryDtoR dto)//R
+    {
+        // private int id;
+        // private int quantity;
+        return  DishToDelivery
+                .builder()
+                .id(dto.getId())
+                .quantity(dto.getQuantity())
+                .build();
+    }
+
+    public DishToDeliveryDtoWFull personToDtoWFull(DishToDelivery d)
+    {
+        // private Dish dish;
+        // private Delivery delivery;
+        return  DishToDeliveryDtoWFull
+                .builder()
+                .id(d.getId())
+                .quantity(d.getQuantity())
+                .dish(d.getDish())
+                .delivery(d.getDelivery())
+                .build();
+    }
 }
--- src/main/java/com/generation/javeat/model/dtoservices/MenuConverter.java
+++ src/main/java/com/generation/javeat/model/dtoservices/MenuConverter.java
@@ -3,9 +3,33 @@ package com.generation.javeat.model.dtoservices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.generation.javeat.model.dto.menu.MenuDtoR;
+import com.generation.javeat.model.dto.menu.MenuDtoWFull;
+import com.generation.javeat.model.entities.Menu;
 import com.generation.javeat.model.repositories.MenuRepository;
 @Service
 public class MenuConverter {
     @Autowired
     MenuRepository mRepo;
+
+        public Menu dtoRToPerson(MenuDtoR dto)//R
+    {
+        // private int id;
+        return  Menu
+                .builder()
+                .id(dto.getId())
+                .build();
+    }
+
+    public MenuDtoWFull personToDtoWFull(Menu m)
+    {
+        // private Set<Dish> dishes;
+        // private Restaurant restaurant;
+        return  MenuDtoWFull
+                .builder()
+                .id(m.getId())
+                .dishes(m.getDishes())
+                .restaurant(m.getRestaurant())
+                .build();
+    }
 }
