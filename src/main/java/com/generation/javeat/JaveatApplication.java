f8cb4720b276be93479d92d398ff0c2a05ab35c3
ErnestoPM <ernestoparadisimiconi@gmail.com>
2024-02-27 10:46:46 +0100

    first commit

---
+++ src/main/java/com/generation/javeat/JaveatApplication.java
@@ -0,0 +1,13 @@
+package com.generation.javeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaveatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaveatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/javeat/JaveatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.javeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaveatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
