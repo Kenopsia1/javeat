56be43410cb0ee10f5a3e0affa34528ea4985eab
ErnestoPM <ernestoparadisimiconi@gmail.com>
2024-02-27 14:14:45 +0100

    Converter2

--- src/main/java/com/generation/javeat/dtoservices/DishConverter.java
+++ src/main/java/com/generation/javeat/dtoservices/DishConverter.java
@@ -1,5 +1,11 @@
 package com.generation.javeat.dtoservices;
 
-public class DishConverter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.generation.javeat.repositories.DishRepository;
+@Service
+public class DishConverter {
+    @Autowired
+    DishRepository dRepo;
 }
