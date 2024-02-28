d4c0df5fc06453410f057500d836ae1f82221249
Federico Galuppi <locker9662@gmail.com>
2024-02-28 10:44:05 +0100

    log

--- src/main/java/com/generation/javeat/controllers/UserContoller.java
+++ src/main/java/com/generation/javeat/controllers/UserContoller.java
@@ -1,9 +1,20 @@
 package com.generation.javeat.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.javeat.model.dto.user.UserDtoLogin;
+import com.generation.javeat.model.dto.user.UserDtoWFull;
 import com.generation.javeat.model.dtoservices.UserConverter;
+import com.generation.javeat.model.entities.User;
 import com.generation.javeat.model.repositories.UserRepository;
 
 @RestController
@@ -13,4 +24,24 @@ public class UserContoller {
     UserConverter uConv;
     @Autowired
     UserRepository uRepo;
+
+    @GetMapping("/users")
+    public List<UserDtoWFull> getAllUsers() 
+    {
+        return uRepo.findAll()
+               .stream()
+               .map(e -> uConv.userToDtoWFull(e))
+               .toList();
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<?> login(@RequestBody UserDtoLogin dto ){
+        Optional<User> optUser = uRepo.login(dto.getMail(), dto.getPassword());
+        if (optUser.isPresent()) {
+            return new ResponseEntity<UserDtoWFull>(uConv.userToDtoWFull(optUser.get()),HttpStatus.OK);
+        } else {
+            return new ResponseEntity<String>("User non trovato!",HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 }
--- src/main/java/com/generation/javeat/model/repositories/UserRepository.java
+++ src/main/java/com/generation/javeat/model/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package com.generation.javeat.model.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.javeat.model.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+     @Query(value="SELECT * FROM user WHERE mail= :mail AND password= :password", nativeQuery=true)
+    Optional<User> login (@Param("mail") String mail, @Param ("password") String password);
 }
