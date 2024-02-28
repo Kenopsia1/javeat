2beaa3ceb60543253666762df58eca7c3beeb1b9
Federico Galuppi <locker9662@gmail.com>
2024-02-28 10:18:41 +0100

    A

--- src/main/java/com/generation/javeat/controllers/DishToDeliveryController.java
+++ src/main/java/com/generation/javeat/controllers/DishToDeliveryController.java
@@ -3,14 +3,14 @@ package com.generation.javeat.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.generation.javeat.model.entities.DishToDelivery;
+import com.generation.javeat.model.dtoservices.DishToDeliveryConverter;
 import com.generation.javeat.model.repositories.DishToDeliveryRepository;
 
 @RestController
 public class DishToDeliveryController {
 
     @Autowired
-    DishToDelivery dtdConv;
+    DishToDeliveryConverter dtdConv;
     @Autowired
     DishToDeliveryRepository dtdRepo;
 }
